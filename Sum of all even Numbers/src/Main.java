4ad212afd3ec5c28011158f8e59fba227ca9adf4
Akhilesh Yadav <125851242+AkhileshYadav7007@users.noreply.github.com>
2024-03-15 22:47:26 +0530

    Add files via upload

---
+++ Sum of all even Numbers/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<> ();
+        integers.add (2+5+8+10+15);
+        System.out.println ("Sum of all even Numbers: "+ integers); //output
+    }
+}
